0e17f529a66bf294b77d6860a59915625fb0ba46
Hicham Tachfine <116213933+hichamtachfine@users.noreply.github.com>
2025-09-10 00:52:40 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 from app import create_app
+from flask_sqlalchemy import SQLAlchemy
 
 app = create_app()
+app.config.from_object('config.Config')  # load from config.py
+db = SQLAlchemy(app)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
